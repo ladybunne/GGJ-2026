ef7698ba21a820b1fb1a6edd5c04d509c0625ec2
Ladybunne <git@ladybunne.net>
2026-02-01 14:05:07 +1100

    Add attire variations

--- KaraStuff/Dancer.gd
+++ KaraStuff/Dancer.gd
@@ -17,6 +17,23 @@ const HAT_PREFABS: Array[PackedScene] = [
 	preload("res://Riley/Prefabs/Hairs & Hats/P_hat_3.tscn"),
 ]
 
+const SKIRT_PREFABS: Array[PackedScene] = [
+	preload("res://Riley/Prefabs/Bottoms/P_skirt_1.tscn"),
+	preload("res://Riley/Prefabs/Bottoms/P_skirt_2.tscn"),
+	preload("res://Riley/Prefabs/Bottoms/P_skirt_3.tscn"),
+]
+
+const DRESS_TOP_PREFABS: Array[PackedScene] = [
+	preload("res://Riley/Prefabs/Tops/P_top_1.tscn"),
+	preload("res://Riley/Prefabs/Tops/P_top_2.tscn"),
+	preload("res://Riley/Prefabs/Tops/P_top_3.tscn"),
+]
+
+const SHIRT_PREFABS: Array[PackedScene] = [
+	preload("res://Riley/Prefabs/Tops/P_shirt_1.tscn"),
+	preload("res://Riley/Prefabs/Tops/P_shirt_2.tscn")
+]
+
 const HAIR_COLOURS: Array[Color] = [
 	Color("efefc6")
 ]
@@ -26,6 +43,8 @@ const HAT_COLOURS: Array[Color] = [
 ]
 
 var hair_hat_mesh: MeshInstance3D
+var attire_top_mesh: MeshInstance3D
+var attire_bottom_mesh: MeshInstance3D
 
 var attire_darken_factor = 0.4
 
@@ -54,28 +73,39 @@ func update_attire() -> void:
 		if hair_hat_mesh != null:
 			hair_hat_mesh.free()
 		if dress:
+			# Hide existing top and skirt
+			$P_CH_Dress/top.visible = false
+			$P_CH_Dress/skirt.visible = false
 			# Hair
 			hair_hat_mesh = HAIR_PREFABS[RNG.rng.randi_range(0, HAIR_PREFABS.size() - 1)].instantiate()
 			add_child(hair_hat_mesh)
+			# Dress top
+			attire_top_mesh = DRESS_TOP_PREFABS[RNG.rng.randi_range(0, DRESS_TOP_PREFABS.size() - 1)].instantiate()
+			add_child(attire_top_mesh)
+			# Dress bottom
+			attire_bottom_mesh = SKIRT_PREFABS[RNG.rng.randi_range(0, SKIRT_PREFABS.size() - 1)].instantiate()
+			add_child(attire_bottom_mesh)
 			# Colour
-			var skirt = $P_CH_Dress/skirt as MeshInstance3D
-			var skirt_material = skirt.material_override as StandardMaterial3D
-			skirt_material.albedo_color = mask_data.colour.darkened(attire_darken_factor)
-			var top = $P_CH_Dress/top as MeshInstance3D
-			var top_material = top.material_override as StandardMaterial3D
-			top_material.albedo_color = mask_data.colour.darkened(attire_darken_factor)
+			var dress_material = StandardMaterial3D.new()
+			dress_material.albedo_color = mask_data.colour.darkened(attire_darken_factor)
+			attire_top_mesh.get_child(1).material_override = dress_material
+			attire_bottom_mesh.material_override = dress_material
 			# Hair colour
 			var hair_material = StandardMaterial3D.new()
 			hair_material.albedo_color = HAIR_COLOURS[RNG.rng.randi_range(0, HAIR_COLOURS.size() - 1)]
 			hair_hat_mesh.material_override = hair_material
 		else:
+			# No need to hide existing top for suit
 			# Hat
 			hair_hat_mesh = HAT_PREFABS[RNG.rng.randi_range(0, HAT_PREFABS.size() - 1)].instantiate()
 			add_child(hair_hat_mesh)
+			# Shirt
+			attire_top_mesh = SHIRT_PREFABS[RNG.rng.randi_range(0, SHIRT_PREFABS.size() - 1)].instantiate()
+			add_child(attire_top_mesh)
 			# Colour
-			var top = $P_CH_Suit/top as MeshInstance3D
-			var top_material = top.material_override as StandardMaterial3D
-			top_material.albedo_color = mask_data.colour.darkened(attire_darken_factor)
+			var shirt_material = StandardMaterial3D.new()
+			shirt_material.albedo_color = mask_data.colour.darkened(attire_darken_factor)
+			attire_top_mesh.get_child(1).material_override = shirt_material
 			# Hat colour
 			var hat_material = StandardMaterial3D.new()
 			hat_material.albedo_color = HAT_COLOURS[RNG.rng.randi_range(0, HAT_COLOURS.size() - 1)]
