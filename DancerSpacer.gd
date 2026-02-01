e4c64ff60a1ef54879bbdb4c6508d234b05d63c0
Lochlan McDonald <lochlanpmc@gmail.com>
2026-02-01 14:29:03 +1100

    Dancer moving in game with patterns WIP

--- DancerSpacer.gd
+++ DancerSpacer.gd
@@ -30,16 +30,19 @@ var timer : float
 
 var danceGrid : DanceGrid
 
+var dancerPairCount : int
+
 #var clockTime : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	dancers = get_children()
 
-	fromDancePositions.resize(dancers.size())
-	toDancePositions.resize(dancers.size())
-	fromDancePositions.resize(dancers.size())
-	toDancePositions.resize(dancers.size())
+	dancerPairCount = dancers.size() / 2
+	fromDancePositions.resize(dancerPairCount)
+	toDancePositions.resize(dancerPairCount)
+	fromWorldPositions.resize(dancerPairCount)
+	toWorldPositions.resize(dancerPairCount)
 	
 	danceGrid = DanceGrid.new(4, 4);
 	
@@ -56,7 +59,7 @@ func _process(delta: float) -> void:
 	if (currentlyMoving):
 		var lerpPercent : float
 		if (timer > 0.0):
-			lerpPercent = -timer / timeToMove
+			lerpPercent = timer / timeToMove
 		else:
 			currentlyMoving = false
 			# Plus the timer to ensure there isn't "leftover time", in consideration of syncing the music
@@ -65,9 +68,11 @@ func _process(delta: float) -> void:
 			fromWorldPositions = toWorldPositions.duplicate()
 			fromDancePositions = toDancePositions.duplicate()
 			# new positions will be calulated when switching back to move
-		for i : int in(dancers.size()):
-			var dancer = dancers[i]
-			var newPos = lerp(fromWorldPositions[i], toWorldPositions[i], lerpPercent)
+		for i : int in range(dancerPairCount):
+			var index : int = i
+			var dancer = dancers[i * 2]
+			
+			var newPos = lerp(fromWorldPositions[index], toWorldPositions[index], lerpPercent)
 			dancer.transform.origin = newPos
 	else: # Currently dancing
 		if (timer <= 0.0):
@@ -76,12 +81,25 @@ func _process(delta: float) -> void:
 			# Get new dance positions
 			var playerDancePos = toDancePositions[playerChildIndex]
 			var patterns = danceGrid.GetPatternsFor(playerDancePos, playerMovement)
+			if (patterns.size() == 0):
+				patterns = danceGrid.GetPatternsFor(playerDancePos, DancePattern.DanceDirection.up)
+			if (patterns.size() == 0):
+				patterns = danceGrid.GetPatternsFor(playerDancePos, DancePattern.DanceDirection.down)
+			if (patterns.size() == 0):
+				patterns = danceGrid.GetPatternsFor(playerDancePos, DancePattern.DanceDirection.left)
+			if (patterns.size() == 0):
+				patterns = danceGrid.GetPatternsFor(playerDancePos, DancePattern.DanceDirection.right)
+				
 			# should be a random one that is avalible, will just do the first one for now
 			var pattern = patterns[0]
-			for i : int in dancers.size():
-				var direction = pattern.GetTowardDirectionAt(toDancePositions[i])
-				toDancePositions[i] = DancePattern.DirectionOffsets[direction]
-			
+			for i : int in range(dancerPairCount):
+				var index = i
+				var direction = pattern.GetTowardDirectionAt(toDancePositions[index])
+				var newDancePos = toDancePositions[index] + DancePattern.DirectionOffsets[direction]
+				toDancePositions[index] = newDancePos
+				var worldPos = DancePosToWorld(newDancePos, false)
+				toWorldPositions[index] = worldPos
+
 func DancePosToWorld(dancePos : Vector2i, first : bool) -> Vector3:
 	var halfTotalWidth : float = widthBetweenPairs * (gridCols - 1) / 2.0
 	var halfTotalHeight : float = heightBetweenPairs * (gridRows - 1) / 2.0
@@ -94,12 +112,18 @@ func DancePosToWorld(dancePos : Vector2i, first : bool) -> Vector3:
 	x -= halfTotalWidth
 	z -= halfTotalHeight
 	return Vector3(x, 0.0, z);
-	
 
 func ArrangeIntoPositions() -> void:
 	for i : int in(dancers.size()):
 		var col : int = (i / 2)  % gridCols
 		var row : int = (i / 2) / gridCols
 		var first : bool = i % 2 == 1;
-		var worldPos = DancePosToWorld(Vector2i(col, row), first)
+		var dancePos : Vector2i = Vector2i(col, row)
+		var worldPos = DancePosToWorld(dancePos, first)
 		dancers[i].transform.origin = worldPos;
+		if (first):
+			var pairIndex : int = i / 2;
+			toDancePositions[pairIndex] = dancePos
+			toWorldPositions[pairIndex] = worldPos
+			fromDancePositions[pairIndex] = dancePos
+			fromWorldPositions[pairIndex] = worldPos
